97a35928ca36a93d34bf683ac2352c03015a390c
Aayush <aayushbastola772@gmail.com>
2025-07-03 14:23:22 +0545

    added classes for scoreboard and user

---
+++ src/Scoreboard.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Scoreboard {
+
+    private int scoreID;
+    private int score;
+    private Date playedDate;
+
+    public Scoreboard(int scoreID, int score, Date playedDate) {
+        this.scoreID = scoreID;
+        this.score = score;
+        this.playedDate = playedDate;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,6 @@
+public class User {
+
+    private boolean isGameMaster;
+    private String username, password;
+
+}
