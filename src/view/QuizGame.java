2eb7eda2bc939876cafcee10e5546b6ae07a6d99
Aayush <aayushbastola772@gmail.com>
2025-07-06 13:11:37 +0545

    sign up for player

--- src/view/QuizGame.java
+++ src/view/QuizGame.java
@@ -1,23 +1,26 @@
 package view;
 
+import controller.UserController;
+
 import java.util.Scanner;
 
 public class QuizGame {
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        System.out.println("Are you ready for the quiz!!");
-        System.out.println("Enter 1: Sign Up\nEnter 2: Log In");
+        UserController userController = new UserController();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Are you ready for the quizzz!!");
+        System.out.println("Enter 1: Sign Up");
+        System.out.println("Enter 2: Log In");
         System.out.println("Enter 3: Exit");
-        int option= Integer.parseInt(input.nextLine());
-
-        if(option==1){
+        int option = Integer.parseInt(scan.nextLine());
+        if(option == 1){
             System.out.println("Enter username: ");
-            String username= input.nextLine();
-            System.out.println("Enter password");
-            String password= input.nextLine();
+            String username = scan.nextLine();
+            System.out.println("Enter password: ");
+            String password = scan.nextLine();
+            if(userController.signUp(username, password)){
+                System.out.println("Signed Up successfully");
+            }
         }
-
-
-
     }
 }
