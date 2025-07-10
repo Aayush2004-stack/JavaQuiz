d7a483a573b01811a9ce8af689a6aa815317694b
Aayush <aayushbastola772@gmail.com>
2025-07-11 01:07:36 +0545

    added while loop for game menu

--- src/view/GameMenu.java
+++ src/view/GameMenu.java
@@ -6,18 +6,27 @@ import java.util.Scanner;
 public class GameMenu {
 
     public static  void showMenu(){
-        UserController userController = new UserController();
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Are you ready for the quizzz!!");
-        System.out.println("Enter 1: Sign Up");
-        System.out.println("Enter 2: Log In");
-        System.out.println("Enter 3: Exit");
-        int option = Integer.parseInt(scan.nextLine());
-        if(option == 1){
-            SignUpView.showSignUpView();
-        } else if (option==2) {
-            LogInView.showLogInView();
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Are you ready for the quizzz!!");
+            System.out.println("Enter 1: Sign Up");
+            System.out.println("Enter 2: Log In");
+            System.out.println("Enter 3: Exit");
+            int option = Integer.parseInt(input.nextLine());
+            if(option == 1){
+                SignUpView.showSignUpView();
+            } else if (option==2) {
+                LogInView.showLogInView();
+
+            }
+            else if(option==3){
+                System.out.println("Thank you for playing the quiz");
+            }
+            else{
+                System.out.println("Please enter the valid options only");
+            }
 
         }
+
     }
 }
