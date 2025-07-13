6a0c6dae2a96abc25affdb7d2f2b3f47fe186670
Aayush <aayushbastola772@gmail.com>
2025-07-13 22:15:16 +0545

    Added try catch for the game menu options

--- src/view/GameMenu.java
+++ src/view/GameMenu.java
@@ -11,20 +11,26 @@ public class GameMenu {
             System.out.println("Enter 1: Sign Up");
             System.out.println("Enter 2: Log In");
             System.out.println("Enter 3: Exit");
-            int option = Integer.parseInt(input.nextLine());
-            if(option == 1){
-                SignUpView.showSignUpView();
-            } else if (option==2) {
-                LogInView.showLogInView();
+            try{
+                int option = Integer.parseInt(input.nextLine());
+                if(option == 1){
+                    SignUpView.showSignUpView();
+                } else if (option==2) {
+                    LogInView.showLogInView();
 
+                }
+                else if(option==3){
+                    System.out.println("Thank you for playing the quiz");
+                    break;
+                }
+                else{
+                    System.out.println("Please enter the valid options only");
+                }
+
+            } catch (RuntimeException e) {
+                System.out.println("Please enter only the number");
             }
-            else if(option==3){
-                System.out.println("Thank you for playing the quiz");
-                break;
-            }
-            else{
-                System.out.println("Please enter the valid options only");
-            }
+
 
         }
 
